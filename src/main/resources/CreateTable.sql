4a96c9c2a6a595b0d98580ac72a487c0a19abe92
joeyanny <joeyanny05@hotmail.co.uk>
2020-10-30 10:12:04 +0000

    Add missing SQL script for creating DB table

---
+++ src/main/resources/CreateTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS user
+(
+    user_id INT AUTO_INCREMENT,
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    house_number VARCHAR(20),
+    street VARCHAR(20),
+    city VARCHAR(20),
+    postcode VARCHAR(20)
+);
